b3978aacf472be6d43ffbcf76bc4224664f71310
dmitri_shepelev <shepelev.dm@gmail.com>
2020-12-23 23:59:01 +0300

    Done

--- FilterTask/ArrayExtension.cs
+++ FilterTask/ArrayExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FilterTask
 {
@@ -14,11 +15,55 @@ namespace FilterTask
         /// <exception cref="ArgumentException">Thrown when array is empty.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when digit value is out of range (0..9).</exception>
         /// <example>
-        /// {1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17}  => { 7, 70, 17 } for digit = 7
+        /// {1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17}  => { 7, 70, 17 } for digit = 7.
         /// </example>
         public static int[] FilterByDigit(int[] source, int digit)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            if (source is null)
+            {
+                throw new ArgumentNullException($"Array can not be null.");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"Array can not be empty.");
+            }
+
+            if (digit < 0 || digit > 9)
+            {
+                throw new ArgumentOutOfRangeException($"Expected digit can not be less than zero and more than nine.");
+            }
+
+            var list = new List<int>();
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == digit)
+                {
+                    list.Add(source[i]);
+                }
+                else if (IsContainsDigit(source[i], digit))
+                {
+                    list.Add(source[i]);
+                }
+            }
+
+            return list.ToArray();
+        }
+
+        private static bool IsContainsDigit(int number, int digit)
+        {
+            number = Math.Abs(number);
+            while (number > 0)
+            {
+                if (number % 10 == digit)
+                {
+                    return true;
+                }
+
+                number /= 10;
+            }
+
+            return false;
         }
     }
 }
